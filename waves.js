64e739c9c34a42490c250a015b43e0114eb5bb79
Arjun Patel <arjunnipatel@gmail.com>
2025-12-23 11:17:42 -0500

    resizing

--- waves.js
+++ waves.js
@@ -1,5 +1,5 @@
 var hscale = 1
-var points = 1000;
+var points = 1500;
 var numWaves = 30;
 var spikes = 500;
 var arr = [];
@@ -7,10 +7,10 @@ var colors = [];
 function setup() {
 
   var cnv = createCanvas(windowWidth, hscale*windowHeight);
-  frameRate(24);
+  frameRate(18);
   cnv.position(0,0 * windowHeight);
-
- numWaves = hscale*windowHeight / 10;
+  points = windowWidth;  
+ numWaves = hscale*windowHeight / 8;
  for(var i = 0; i < spikes; i++) {
   arr.push(Math.random());
 }
@@ -24,9 +24,9 @@ function draw() {
   background(255,255,255);
   for (var l = 0; l < numWaves; l++) {
     stroke(colors[0]);
-    strokeWeight(.4);
-    beginShape(QUADS);
-    for (var x = -points / 2; x <= 3*points /2; x += 5) {
+    strokeWeight(.5);
+    beginShape(POINTS);
+    for (var x = -10; x <= points; x += 8) {
       // Big
       var freq1 = 0.8;
       var amp1 = 4; 
@@ -35,16 +35,16 @@ function draw() {
       var freq2 = 2;
       var amp2 = 2  ;//10*Math.random();
       var phase2 = t * (0.3 + l * 0.6) + l * 5;
-      var y = 10*(l + 1) + 
+      var y = 8*( l + 1) + 
       amp1 * Math.sin(Math.PI/180* (freq1 * x + phase1)) +
-      amp2 * Math.sin(Math.PI/180*(freq2 * x + phase2));
+      amp2 * Math.cos(Math.PI/180*(freq2 * x + phase2));
       
-      vertex(x, y + 5*arr[Math.abs((x + l*3) % spikes)]);
+      vertex(x, y + 2*arr[Math.abs((x + l*3) % spikes)]);
     }
     endShape();
   }
   t += change;
-  if(Math.abs(t) > 10000) {
+  if(Math.abs(t) > 1000) {
       change = -1*change;
   }
   arr[Math.abs(Math.floor(Math.random()*spikes) % spikes)] = Math.random();
@@ -53,4 +53,5 @@ function draw() {
 function windowResized() {
   cnv = resizeCanvas(windowWidth, hscale*windowHeight);
   numWaves = hscale*windowHeight / 10;
+  points = windowWidth;
 }
